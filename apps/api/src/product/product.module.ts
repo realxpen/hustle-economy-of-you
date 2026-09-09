b98b47a2879f5893c4c1d7ba8790f01525d007b0
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-09 11:06:32 +0100

    feat: register public product endpoint

--- apps/api/src/product/product.module.ts
+++ apps/api/src/product/product.module.ts
@@ -3,11 +3,13 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { OwnerProductController } from "./product.controller";
 import { ProductService } from "./product.service";
+import { PublicProductController } from "./public-product.controller";
+import { PublicProductService } from "./public-product.service";
 
 @Module({
   imports: [AuthModule],
-  controllers: [OwnerProductController],
-  providers: [ProductService],
+  controllers: [OwnerProductController, PublicProductController],
+  providers: [ProductService, PublicProductService],
   exports: [ProductService]
 })
 export class ProductModule {}
