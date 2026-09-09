083d38d53512c736884cf6d47ef7346714c2297e
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-09 12:48:46 +0100

    feat: register post interaction API

--- apps/api/src/post/post.module.ts
+++ apps/api/src/post/post.module.ts
@@ -2,12 +2,24 @@ import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
 import { OwnerPostController, PublicPostController } from "./post.controller";
+import {
+  AuthenticatedPostInteractionController,
+  PublicPostInteractionController,
+  UserFollowController
+} from "./post-interaction.controller";
+import { PostInteractionService } from "./post-interaction.service";
 import { PostService } from "./post.service";
 
 @Module({
   imports: [AuthModule],
-  controllers: [OwnerPostController, PublicPostController],
-  providers: [PostService],
-  exports: [PostService]
+  controllers: [
+    OwnerPostController,
+    PublicPostController,
+    PublicPostInteractionController,
+    AuthenticatedPostInteractionController,
+    UserFollowController
+  ],
+  providers: [PostService, PostInteractionService],
+  exports: [PostService, PostInteractionService]
 })
 export class PostModule {}
