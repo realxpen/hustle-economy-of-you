db129404d36b26e51272bb584962abadd39b1503
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-09 13:40:23 +0100

    feat: register discovery feed module

---
+++ apps/api/src/feed/feed.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+
+import { AuthModule } from "../auth/auth.module";
+import { FeedController } from "./feed.controller";
+import { FeedService } from "./feed.service";
+
+@Module({
+  imports: [AuthModule],
+  controllers: [FeedController],
+  providers: [FeedService],
+  exports: [FeedService]
+})
+export class FeedModule {}
