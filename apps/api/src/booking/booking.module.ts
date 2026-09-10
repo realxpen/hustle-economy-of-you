3cbf21364f839e9c36e724b4c85f665bddea42a2
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-10 14:57:52 +0100

    feat: register booking schedule service

--- apps/api/src/booking/booking.module.ts
+++ apps/api/src/booking/booking.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
 import { BookingController } from "./booking.controller";
+import { BookingScheduleService } from "./booking-schedule.service";
 import { BookingService } from "./booking.service";
 
 @Module({
   imports: [AuthModule],
   controllers: [BookingController],
-  providers: [BookingService],
+  providers: [BookingService, BookingScheduleService],
   exports: [BookingService]
 })
 export class BookingModule {}
