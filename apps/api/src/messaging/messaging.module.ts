ddef5f42064329bf59447b7edd9e18dc220ea8ed
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-10 12:04:36 +0100

    fix: import AuthModule into MessagingModule

--- apps/api/src/messaging/messaging.module.ts
+++ apps/api/src/messaging/messaging.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { AuthModule } from "../auth/auth.module";
 import { MessagingController } from "./messaging.controller";
 import { MessagingService } from "./messaging.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [MessagingController],
   providers: [MessagingService]
 })
