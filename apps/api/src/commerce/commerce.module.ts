cc08680fd8b14f093f3321f830425be1108084e1
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-11 11:44:29 +0100

    feat: wire Phase 12 fulfillment service

--- apps/api/src/commerce/commerce.module.ts
+++ apps/api/src/commerce/commerce.module.ts
@@ -3,11 +3,12 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { CartController, OrderController } from "./commerce.controller";
 import { CommerceService } from "./commerce.service";
+import { FulfillmentService } from "./fulfillment.service";
 
 @Module({
   imports: [AuthModule],
   controllers: [CartController, OrderController],
-  providers: [CommerceService],
-  exports: [CommerceService]
+  providers: [CommerceService, FulfillmentService],
+  exports: [CommerceService, FulfillmentService]
 })
 export class CommerceModule {}
