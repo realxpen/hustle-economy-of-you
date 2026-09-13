dc81d6d8ecfdf9262d9563c51d464a596322370e
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-13 12:29:03 +0100

    feat: register order refund service

--- apps/api/src/commerce/commerce.module.ts
+++ apps/api/src/commerce/commerce.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
 import { CartController, OrderController } from "./commerce.controller";
+import { CommerceFinancialService } from "./commerce-financial.service";
 import { CommerceService } from "./commerce.service";
 import { FulfillmentService } from "./fulfillment.service";
 
 @Module({
   imports: [AuthModule],
   controllers: [CartController, OrderController],
-  providers: [CommerceService, FulfillmentService],
-  exports: [CommerceService, FulfillmentService]
+  providers: [CommerceService, FulfillmentService, CommerceFinancialService],
+  exports: [CommerceService, FulfillmentService, CommerceFinancialService]
 })
 export class CommerceModule {}
