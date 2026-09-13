3f5a2f6a9227fdcccad8f5f634bbdf97c106264f
Ayomide Oladeji <69223421+realxpen@users.noreply.github.com>
2026-09-13 12:28:23 +0100

    feat: register booking refund service

--- apps/api/src/booking/booking.module.ts
+++ apps/api/src/booking/booking.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
 import { BookingController } from "./booking.controller";
+import { BookingFinancialService } from "./booking-financial.service";
 import { BookingScheduleService } from "./booking-schedule.service";
 import { BookingService } from "./booking.service";
 
 @Module({
   imports: [AuthModule],
   controllers: [BookingController],
-  providers: [BookingService, BookingScheduleService],
-  exports: [BookingService]
+  providers: [BookingService, BookingScheduleService, BookingFinancialService],
+  exports: [BookingService, BookingFinancialService]
 })
 export class BookingModule {}
